da29b5b079def172a6e340df8be16b00e5666886
Michał Przybyś <michal@przybys.eu>
2023-08-22 16:50:50 +0200

    Refactor code (#4)
    * Finished refactoring device_provider directory.
    * Refactored ubus_client.

--- custom_components/ubus_advanced/model/device.py
+++ custom_components/ubus_advanced/model/device.py
@@ -14,8 +14,8 @@
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from .hostapd_device_provider import HostapdDeviceProvider
+from typing import NamedTuple
 
-__all__ = [
-    'HostapdDeviceProvider'
-]
+class Device(NamedTuple):
+    mac: str
+    ssid: str
